387854bd555836af4541c3b432d15931493372c0
mazeltovik <mazeltovik@gmail.com>
2024-07-15 19:50:20 +0300

    feat: add basic adaptive

--- src/pages/roomPage/roomPage.tsx
+++ src/pages/roomPage/roomPage.tsx
@@ -7,7 +7,10 @@ import './roomPage.scss';
 //MUI
 
 //Components
-
+import GameTab from '../../components/GameTab/GameTab';
+import CountdownTimer from '../../components/countdownTimer/countdownTimer';
+import Paper from '@mui/material/Paper';
+import { ThemeProvider } from '@mui/material/styles';
 //React
 import { useState, useEffect, useCallback, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -22,5 +25,34 @@ import theme from '../../helpers/authTheme';
 import { socket } from '../../socket/socket';
 
 export default function RoomPage() {
-  return <p>Hello</p>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Paper
+        elevation={3}
+        sx={{
+          width: '90%',
+          mt: '1rem',
+          mb: '1rem',
+          padding: '1rem',
+          bgcolor: 'brown.light',
+          position: 'relative',
+          height: '95vh',
+        }}
+      >
+        <div className="game_container">
+          <div className="timer_container">
+            {/* <CountdownTimer
+              seconds={30}
+              size={200}
+              strokeBgColor={'#324d67'}
+              strokeColor={'#c5e4e7'}
+              strokeWidth={10}
+            /> */}
+          </div>
+          <div className="gameplay_container">Gameplay</div>
+          <GameTab />
+        </div>
+      </Paper>
+    </ThemeProvider>
+  );
 }
